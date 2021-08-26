62a2da2f4054e0041205cd06ae820b98f6ad84fb
Glitch (glitch-hello-node) <none>
2021-08-26 15:29:44 +0000

    🎻🏋 Checkpoint ./README.md:34833427/3304 ./server.js:34833427/8386 ./package.json:34833427/4153 ./src/client/index.js:34833427/1581

--- README.md
+++ README.md
@@ -1,42 +1,22 @@
-# Hello Node!
+# NOTE: FOR NOW, THIS PACKAGE NOT USEABLE.
+# Zhat.js
+```
+Official API wrapper for https://zhat.cf
+```
 
-This project includes a Node.js server script and a web page that connects to it. The front-end page presents a form the visitor can use to submit a color name, sending the submitted value to the back-end API running on the server. The server returns info to the page that allows it to update the display with the chosen color. 🎨
+# Install to use
+```
+npm i zhat.js
+```
 
-[Node.js](https://nodejs.org/en/about/) is a popular runtime that lets you run server-side JavaScript. This project uses the [Fastify](https://www.fastify.io/) framework and explores basic templating with [Handlebars](https://handlebarsjs.com/).
+# Example
+```
+const Zhat = require("zhat.cf");
+const client = new Zhat.Client();
 
-## Prerequisites
+client.on("ready", () => {
+  console.log(`Login as ${client.user.username}`)
+});
 
-You'll get best use out of this project if you're familiar with basic JavaScript. If you've written JavaScript for client-side web pages this is a little different because it uses server-side JS, but the syntax is the same!
-
-## What's in this project?
-
-← `README.md`: That’s this file, where you can tell people what your cool website does and how you built it.
-
-← `public/style.css`: The styling rules for the pages in your site.
-
-← `server.js`: The **Node.js** server script for your new site. The JavaScript defines the endpoints in the site back-end, one to return the homepage and one to update with the submitted color. Each one sends data to a Handlebars template which builds these parameter values into the web page the visitor sees.
-
-← `package.json`: The NPM packages for your project's dependencies.
-
-← `src/`: This folder holds the site template along with some basic data files.
-
-← `src/pages/index.hbs`: This is the main page template for your site. The template receives parameters from the server script, which it includes in the page HTML. The page sends the user submitted color value in the body of a request, or as a query parameter to choose a random color.
-
-← `src/colors.json`: A collection of CSS color names. We use this in the server script to pick a random color, and to match searches against color names.
-
-← `src/seo.json`: When you're ready to share your new site or add a custom domain, change SEO/meta settings in here.
-
-## Try this next 🏗️
-
-Take a look in `TODO.md` for next steps you can try out in your new site!
-
-___Want a minimal version of this project to build your own Node.js app? Check out [Blank Node](https://glitch.com/edit/#!/remix/glitch-blank-node)!___
-
-![Glitch](https://cdn.glitch.com/a9975ea6-8949-4bab-addb-8a95021dc2da%2FLogo_Color.svg?v=1602781328576)
-
-## You built this with Glitch!
-
-[Glitch](https://glitch.com) is a friendly community where millions of people come together to build web apps and websites.
-
-- Need more help? [Check out our Help Center](https://help.glitch.com/) for answers to any common questions.
-- Ready to make it official? [Become a paid Glitch member](https://glitch.com/pricing) to boost your app with private sharing, more storage and memory, domains and more.
+client.login('token');
+```
--- server.js
+++ server.js
@@ -1,123 +1,83 @@
-/**
-* This is the main Node.js server script for your project
-* Check out the two endpoints this back-end API provides in fastify.get and fastify.post below
-*/
+const options = {
+  gate: "api/bot",
+  v: "v1"
+};
+const ngrok = require("ngrok");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const fetch = require("node-fetch")
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.listen(3030)
 
-const path = require("path");
+const cors = require("cors");
+app.use(cors());
+const EventEmitter = require('events');
 
-// Require the fastify framework and instantiate it
-const fastify = require("fastify")({
-  // Set this to true for detailed logging:
-  logger: false
-});
-
-// ADD FAVORITES ARRAY VARIABLE FROM TODO HERE
-
-
-// Setup our static files
-fastify.register(require("fastify-static"), {
-  root: path.join(__dirname, "public"),
-  prefix: "/" // optional: default '/'
-});
-
-// fastify-formbody lets us parse incoming forms
-fastify.register(require("fastify-formbody"));
-
-// point-of-view is a templating manager for fastify
-fastify.register(require("point-of-view"), {
-  engine: {
-    handlebars: require("handlebars")
+class BaseClient extends EventEmitter {
+  constructor(options = {}) {
+    super();
+    
+    Object.defineProperty(this, 'client', { value: this });
   }
-});
-
-// Load and parse SEO data
-const seo = require("./src/seo.json");
-if (seo.url === "glitch-default") {
-  seo.url = `https://${process.env.PROJECT_DOMAIN}.glitch.me`;
 }
 
-/**
-* Our home page route
-*
-* Returns src/pages/index.hbs with data built into it
-*/
-fastify.get("/", function(request, reply) {
-  
-  // params is an object we'll pass to our handlebars template
-  let params = { seo: seo };
-  
-  // If someone clicked the option for a random color it'll be passed in the querystring
-  if (request.query.randomize) {
+class Client extends BaseClient {
+  constructor(options = {}) {
+    super(options);
     
-    // We need to load our color data file, pick one at random, and add it to the params
-    const colors = require("./src/colors.json");
-    const allColors = Object.keys(colors);
-    let currentColor = allColors[(allColors.length * Math.random()) << 0];
+    this.token = process.env.ZHAT;
+    this.user = {}
     
-    // Add the color properties to the params object
-    params = {
-      color: colors[currentColor],
-      colorError: null,
-      seo: seo
-    };
-  }
-  
-  // The Handlebars code will be able to access the parameter values and build them into the page
-  reply.view("/src/pages/index.hbs", params);
-});
-
-/**
-* Our POST route to handle and react to form submissions 
-*
-* Accepts body data indicating the user choice
-*/
-fastify.post("/", function(request, reply) {
-  
-  // Build the params object to pass to the template
-  let params = { seo: seo };
-  
-  // If the user submitted a color through the form it'll be passed here in the request body
-  let color = request.body.color;
-  
-  // If it's not empty, let's try to find the color
-  if (color) {
-    // ADD CODE FROM TODO HERE TO SAVE SUBMITTED FAVORITES
+    ngrok.connect(3030).then(url => {
+      this.addr = url;
+    })   
     
-    // Load our color data file
-    const colors = require("./src/colors.json");
-    
-    // Take our form submission, remove whitespace, and convert to lowercase
-    color = color.toLowerCase().replace(/\s/g, "");
-    
-    // Now we see if that color is a key in our colors object
-    if (colors[color]) {
+    app.post("/receive", async (req, res) => {
+      if (!req.body) return;
       
-      // Found one!
-      params = {
-        color: colors[color],
-        colorError: null,
-        seo: seo
+      req.body.author.send = function (ctn) {
+        fetch(`https://zhat.cf/api/send/message`, {
+          method: "PATCH",
+          headers: {
+            'Content-Type': "application/json"
+          },
+          body: JSON.stringify({author: req.body.to, content: ctn, to: req.body.author.UID, bot: true, createAt: Date.now() })
+        })
       };
-    } else {
       
-      // No luck! Return the user value as the error property
-      params = {
-        colorError: request.body.color,
-        seo: seo
-      };
-    }
+      this.client.emit("message", req.body);
+    })
   }
   
-  // The Handlebars template will use the parameter values to update the page with the chosen color
-  reply.view("/src/pages/index.hbs", params);
-});
-
-// Run the server and report out to the logs
-fastify.listen(process.env.PORT, function(err, address) {
-  if (err) {
-    fastify.log.error(err);
-    process.exit(1);
+  async login(token = this.token) {
+    fetch(`https://zhat.cf/${options.gate}/${options.v}/findbot`, {
+      method: "POST",
+      headers: {
+            'Content-Type': 'application/json',
+            },
+      body: JSON.stringify({ token })
+    }).then(res => res.json()).then(data => {
+    fetch(`https://zhat.cf/${options.gate}/${options.v}/connect`, {
+      method: "POST",
+      headers: {
+            'Content-Type': 'application/json',
+            },
+          body: JSON.stringify({
+            uid: data.uid,
+            addr: this.client.addr
+          })
+        }).then(res => res.json()).then(data2 => {
+        this.client.user = data2;
+        this.client.emit("ready", data2)
+    })      
+    })
   }
-  console.log(`Your app is listening on ${address}`);
-  fastify.log.info(`server listening on ${address}`);
-});
+}
+
+
+module.exports = {
+  Client,
+  BaseClient
+};
